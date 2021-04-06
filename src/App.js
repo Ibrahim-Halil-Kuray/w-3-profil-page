4f640bd3db14e92ffe9aeea8d3ff58e98448301c
Halil-Kuray <halibram33@gmail.com>
2021-04-06 23:14:56 +0200

    deneme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
+//bir profil sayfasi olusturulmasi isteniyor odevimizde.
 import logo from './logo.svg';
 import './App.css';
+
+import Profile from "./component/profile"//istenen sayfayi olusturacak foksiyonu import ettik
+//asagida uygun yere yerlestiriyoruz App icerisine
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Profile/>    
       </header>
     </div>
   );
---
+++ src/component/profile.js
@@ -0,0 +1,59 @@
+//profil sayfamizi olusturacak fonsiyonu yazip export ediyoruz.
+export default function Profile(){
+    return(
+        <main>
+
+    <section>
+
+            <p> 
+            Adım Halil Güven. 1990 yılında İstanbul'da dünyaya geldim. 30 yaşındayım, 
+            boyum 1.75 kilom 79 kilogramdır. Babamın adı Hasan, annemin adı ise Sevda'dır.
+             Şuan avukatlık mesleğimi yapmaktayım.
+             İşimi oldukça çok seviyorum.
+            Spor yapmayı, kitap okumayı, alışveriş yapmayı ve yüzmeyi çok severim.
+             En sevdiğim yemekler ise mantı, lahmacun, patates kızartması ve köftedir.
+              Hobilerim paraşüte binmek, araba sürmek ve spor yapmak ve ata binmektir.
+               Fobilerim ise yükseklik ve karanlıkta kalmaktır.
+            Hayvanları çok severim.En sevdiğim müzik türü Yabancı 
+             pop müziklerdir. Hayatta en büyük idealim ise başarılı bir hakim olmaktır.
+             </p>
+
+    </section>
+
+    <table>
+
+        <tr>
+            <td>Ad</td>
+            <td>Halil</td>
+        </tr>
+
+        <tr>
+            <td>Soyad</td>
+            <td>Güven</td>
+        </tr>
+
+        <tr>
+            <td>Yas</td>
+            <td>30</td>
+        </tr>
+
+        <tr>
+            <td>Cinsiyet</td>
+            <td>Erkek</td>
+        </tr>
+
+        <tr>
+            <td>Meslek</td>
+            <td>Avukat</td>
+        </tr>
+
+        <tr>
+            <td>Hobiler</td>
+            <td>Paraşüte binmek, araba sürmek ve spor yapmak ve ata binmektir</td>
+        </tr>
+
+    </table>
+
+</main>
+    )
+}
